b168fdfb5893edba77ea96ce9d7b47903dd569a4
Ryan Dolan <ryanddolan@hotmail.com>
2015-06-04 17:48:52 -0500

    some basic unit tests

---
+++ GrosvnerMenu/GrosvnerMenu.Test/Controller/When_A_Controller_Processes_Input.cs
@@ -0,0 +1,78 @@
+﻿using GrosvnerMenu.Controller;
+using GrosvnerMenu.Data;
+using GrosvnerMenu.Service;
+using GrosvnerMenu.Test.Service;
+using Moq;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrosvnerMenu.Test.Controller
+{
+    [TestFixture]
+    public class When_A_Controller_Processes_Input
+    {
+        Mock<IMenu> _menu;
+        Mock<IMenuLoader> _loader;
+        Mock<IMenuInputReader> _reader;
+        Mock<IMenuSource> _source;
+        IEnumerable<KeyValuePair<string, string>> _testData;
+
+        [SetUp]
+        public void Initialize()
+        {
+            _menu = new Mock<IMenu>();
+            _menu.SetupGet(m => m.MorningMenu).Returns(new Dictionary<int, string>()
+            {
+                { 1, "eggs" },
+                { 2, "Toast" },
+                { 3, "coffee" },
+                { 4, "Not Applicable" }
+            });
+            _menu.SetupGet(m => m.NightMenu).Returns(new Dictionary<int, string>()
+            {
+                { 1, "steak" },
+                { 2, "potato" },
+                { 3, "wine" },
+                { 4, "cake" }
+            });
+
+            _source = new Mock<IMenuSource>();
+            _source.Setup(x => x.Open()).Returns(new MemoryStream());
+
+            _loader = new Mock<IMenuLoader>();
+            _loader.Setup(x => 
+                          x.Load(_source.Object.Open()))
+                   .Returns(_menu.Object);
+
+            _reader = new Mock<IMenuInputReader>();            
+
+            // load test data
+            using (var stream = new StreamReader(typeof(When_The_MenuInputReader_Is_Called).Assembly.GetManifestResourceStream("GrosvnerMenu.Test.Resources.input-output-sample.json")))
+            {
+                var json = stream.ReadToEnd();
+                var jarray = JArray.Parse(json);
+                _testData = jarray.Select(x => new KeyValuePair<string, string>((string)x["Input"], (string)x["Output"])).ToList();
+            }
+        }
+
+        [Test]
+        public void An_Output_Is_Always_Produced()
+        {
+            var controller = new MenuController(_source.Object, _loader.Object, _reader.Object);
+
+            for (int i=0;i<10000;i++)
+            {
+                var something = i.GetHashCode().ToString();
+                _reader.Setup(x => x.Read(something, _menu.Object)).Returns("Something");
+                var output = controller.ProcessInput(something);
+                Assert.IsNotNullOrEmpty(output);
+            }
+        }
+    }
+}
---
+++ GrosvnerMenu/GrosvnerMenu.Test/Service/When_The_MenuInputReader_Is_Called.cs
@@ -0,0 +1,62 @@
+﻿using GrosvnerMenu.Data;
+using GrosvnerMenu.Service;
+using Moq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrosvnerMenu.Test.Service
+{
+    public class When_The_MenuInputReader_Is_Called
+    {
+        Mock<IMenu> _menu;
+        IEnumerable<KeyValuePair<string, string>> _testData;
+
+        [SetUp]
+        public void Initialize()
+        {
+            _menu = new Mock<IMenu>();
+            _menu.SetupGet(m => m.MorningMenu).Returns(new Dictionary<int, string>()
+            {
+                { 1, "eggs" },
+                { 2, "Toast" },
+                { 3, "coffee" },
+                { 4, "Not Applicable" }
+            });
+            _menu.SetupGet(m => m.NightMenu).Returns(new Dictionary<int, string>()
+            {
+                { 1, "steak" },
+                { 2, "potato" },
+                { 3, "wine" },
+                { 4, "cake" }
+            });
+
+            // load test data
+            using (var stream = new StreamReader(typeof(When_The_MenuInputReader_Is_Called).Assembly.GetManifestResourceStream("GrosvnerMenu.Test.Resources.input-output-sample.json")))
+            {
+                var json = stream.ReadToEnd();
+                var jarray = JArray.Parse(json);
+                _testData = jarray.Select(x => new KeyValuePair<string, string>((string)x["Input"], (string)x["Output"])).ToList();
+            }
+        }
+
+        [Test]
+        public void The_Correct_Output_Is_Produced()
+        {
+            var reader = new MenuInputReader();
+            foreach (var data in _testData)
+            {
+                var output = reader.Read(data.Key, _menu.Object);
+
+                // Not case sensitive - ignore white space
+                Assert.AreEqual(output.ToLower().Replace(" ", ""), data.Value.ToLower().Replace(" ", ""));
+            }
+        }
+    }
+}
