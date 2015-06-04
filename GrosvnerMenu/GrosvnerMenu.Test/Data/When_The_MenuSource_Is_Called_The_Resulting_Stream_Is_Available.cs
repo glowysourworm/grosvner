d6570b8bec7b6720d134e449fa720f8ab4a63d07
Ryan Dolan <ryanddolan@hotmail.com>
2015-06-04 16:53:08 -0500

    nunit; test frame

---
+++ GrosvnerMenu/GrosvnerMenu.Test/Data/When_The_MenuSource_Is_Called_The_Resulting_Stream_Is_Available.cs
@@ -0,0 +1,25 @@
+﻿using GrosvnerMenu.Data;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrosvnerMenu.Test.Data
+{
+    [TestFixture]
+    public class When_The_MenuSource_Is_Called_The_Resulting_Stream_Is_Available
+    {
+        public void For_A_Csv_Menu_Source()
+        {
+            var source = new CsvMenuSource();
+
+            var stream = source.Open();
+            Assert.IsNotNull(stream);
+
+            stream.Close();
+            stream.Dispose();
+        }
+    }
+}
---
+++ GrosvnerMenu/GrosvnerMenu.Test/Service/When_The_MenuLoader_Loads_A_Menu.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using NUnit.Framework;
+using Moq;
+using GrosvnerMenu.Data;
+using System.Collections.Generic;
+using GrosvnerMenu.Service;
+
+namespace GrosvnerMenu.Test.Service
+{
+    /// <summary>
+    /// Purpose of test fixture is to test the result of loading a menu v.s. the expected result
+    /// </summary>
+    [TestFixture]
+    public class When_The_MenuLoader_Loads_A_Menu
+    {
+        Mock<IMenu> _menu;
+
+        [SetUp]
+        public void Initialize()
+        {
+            _menu = new Mock<IMenu>();
+            _menu.SetupGet(m => m.MorningMenu).Returns(new Dictionary<int, string>()
+            {
+                { 1, "eggs" },
+                { 2, "Toast" },
+                { 3, "coffee" },
+                { 4, "Not Applicable" }
+            });
+            _menu.SetupGet(m => m.NightMenu).Returns(new Dictionary<int, string>()
+            {
+                { 1, "steak" },
+                { 2, "potato" },
+                { 3, "wine" },
+                { 4, "cake" }
+            });
+        }
+
+        [Test]
+        public void The_Resulting_Menu_Is_Accurate_For_The_Csv_Loader()
+        {
+            // load stream by hand for this test since we're not testing IMenuSource
+            try
+            {
+                var assembly = typeof(Menu).Assembly;
+                var stream = assembly.GetManifestResourceStream("GrosvnerMenu.Resources.Menu.csv");
+                var loader = new CsvMenuLoader();
+                var menu = loader.Load(stream);
+
+                // Assert each menu item equals the mocked object - NOT CASE SENSITIVE
+                foreach (var keyValuePair in _menu.Object.MorningMenu)
+                {
+                    Assert.IsTrue(menu.MorningMenu.ContainsKey(keyValuePair.Key));
+                    Assert.IsTrue(menu.MorningMenu[keyValuePair.Key].ToLower() == keyValuePair.Value.ToLower());
+                }
+                foreach (var keyValuePair in _menu.Object.NightMenu)
+                {
+                    Assert.IsTrue(menu.NightMenu.ContainsKey(keyValuePair.Key));
+                    Assert.IsTrue(menu.NightMenu[keyValuePair.Key].ToLower() == keyValuePair.Value.ToLower());
+                }
+            }
+            catch(Exception)
+            {
+                Assert.Fail();
+            }
+        }
+    }
+}
